ecda9f625fdbc248a288ae5b7da3f7b6b2919b06
iarmolla <ivan.armolla@gmail.com>
2023-01-25 02:40:56 -0300

    adding styles to form__submit

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -20,10 +20,8 @@ function Home() {
   const [showModal, setShowModal] = useState(false);
 
   return (
-    <main className='lg:h-screen lg:snap-y lg:snap-mandatory lg:overflow-scroll'>
-      {/* <section className='h-screen text-center flex flex-col gap-3 justify-center items-center xl:m-0 one mt-24 mb-10 lg:m-0 '> */}
+    <main className='lg:h-screen lg:snap-y lg:snap-mandatory lg:overflow-scroll'>      
       <section className='text-center flex flex-col gap-3 justify-center items-center xl:m-0 one lg:h-screen mt-24 mb-10 lg:m-0'>
-
         <article className='w-full xl:h-screen  flex flex-col justify-center items-center justify-items-center'>
           <h1 className='text-slate-700 title text-5xl xl:text-9xl'>Iván Armolla</h1>
           <h2 className='text-4xl text-slate-800 xl:text-7xl'>Front-end Developer.</h2>
@@ -45,8 +43,8 @@ function Home() {
           </button>
         </div>
         <article>
-          <p className='text-gray-600 mx-6 md:mx-48 xl:text-2xl' >
-            ¡Hola! mi nombre es Iván y me gusta el mundo de desarrollo web. Empecé a introducirme a este mundo, por mi interes en el desarrollo de videojuegos, que siempre fuí muy apasionado. Al finalizar la secundaria empecé a investigar como desarrollar juegos y me encontre con el desarrollo web, decidi a hacer algunos cursos de HTML y CSS para ir sentando bases en el desarrollo y meterme en la tecnicatura de analisis de sistemas para poder formarme.
+          <p className='text-gray-600 mx-6  md:mx-48 xl:text-2xl' >
+            ¡Hola! mi nombre es Iván y me apasiona el desarrollo web y todo lo relacionado. Empecé a introducirme a este mundo, por mi interes en el desarrollo de videojuegos. Al finalizar la secundaria empecé a investigar como desarrollar juegos y me encontre con el desarrollo web, al final me decidí ir por web y hacer algunos cursos de HTML y CSS para ir sentando bases en el desarrollo y meterme en la tecnicatura de analisis de sistemas para poder formarme.
           </p>
         </article>
         <article className='flex justify-center gap-8 cursor-pointer text-gray-600 py-6 text-3xl'>
@@ -75,7 +73,7 @@ function Home() {
           HABILIDADES
         </h4>
         <section className='rounded-md text-center font-sans'>
-          <div className='flex flex-col justify-center items-center  gap-14 text-8xl uppercase font-medium sm:grid sm:grid-cols-2 lg:grid lg:grid-cols-4'>
+          <div className='flex flex-col justify-center items-center gap-14 text-8xl uppercase font-medium sm:grid sm:grid-cols-2 lg:grid lg:grid-cols-4'>
             <div className='relative shadow-lg p-10 rounded-md border-2 border-gray-200'>
               <span className='absolute text-xl -top-10 left-0 text-orange-500'>Html5</span>
               <FaHtml5 className='text-orange-500'></FaHtml5>
@@ -118,7 +116,7 @@ function Home() {
             <span></span>
             <span></span>
             <span></span>proyectos</h4>
-          <article className='shadow-lg p-3 text-gray-500 rounded-md border-2 border-gray-200 m-3 sm:mx-20 md:mx-20 lg:mx-24 xl:mx-48'>
+          <article className='shadow-lg p-3 text-gray-500 rounded-md border-2 border-gray-200 m-3 sm:mx-20 md:mx-20 lg:mx-24 xl:mx-80'>
             <div className='flex flex-row justify-between'>
               <span className='uppercase text-left pl-3 text-gray-500 font-semibold'>Ecommerce</span>
               <div className='flex flex-row gap-2 text-xl cursor-pointer'>
@@ -131,9 +129,9 @@ function Home() {
               </div>
             </div>
             <div>
-              <figure className='flex flex-col justify-center items-center py-2'>
-                <img src={ecommerce} className="image-project" alt="ecommerce project" />
-              </figure>
+              <a href="https://ecommerce-167.netlify.app/" target={'_blank'} className='flex flex-col justify-center items-center py-2 overflow-hidden'>
+                <img src={ecommerce} className="image-project hover:scale-75" alt="ecommerce project" />
+              </a>
             </div>
             <p className='m-3'>Un ecommerce web con distinas secciones de productos, joyeria, tecnologia, ropa y carrito de compras.</p>
             <p className='pl-3'>En lo que es el desarrollo se uso <span className='text-slate-700 uppercase font-semibold'>react-router-dom</span> para manejar las distinas rutas que tendra el ecommerce. Se implemento <span className='uppercase text-slate-700 font-semibold'>Redux</span> para manejar las acciones y el estado de la web app, en la creacion del formulario se uso de libreria formik y las llamadas a la API con implementacion de axios, para el manejo de estilos se uso <span className='uppercase text-indigo-600 font-semibold'>Bootstrap</span> para simplificar el desarrollo.
@@ -144,7 +142,7 @@ function Home() {
               <span>Fake store API</span>
             </div>
           </article>
-          <article className='shadow-lg p-3 text-gray-500 rounded-md border-2 border-gray-200 m-3 md:mx-20 sm:mx-20 lg:mx-24 xl:mx-48'>
+          <article className='shadow-lg p-3 text-gray-500 rounded-md border-2 border-gray-200 m-3 md:mx-20 sm:mx-20 lg:mx-24 xl:mx-80'>
             <div className='flex flex-row justify-between'>
               <span className='uppercase text-left pl-3 text-gray-500 font-semibold'>Giphy-app</span>
               <div className='flex flex-row gap-2 text-xl cursor-pointer'>
@@ -157,9 +155,9 @@ function Home() {
               </div>
             </div>
             <div>
-              <figure className='flex flex-col justify-center items-center py-2'>
-                <img src={gif} className="image-project" alt="gif project" />
-              </figure>
+              <a href="https://giphy-645.netlify.app/" target={"_blank"} className='flex flex-col justify-center items-center py-2 overflow-hidden'>
+                <img src={gif} className="image-project hover:scale-75" alt="gif project" />
+              </a>
             </div>
             <p className='m-3'>Un clon de uno de los buscadores de gifs mas populares.</p>
             <p className='pl-3'>En lo que es el desarrollo para las llamadas a la API se uso <span className='uppercase text-slate-600 font-semibold'>axios,</span> para el manejo de estilos se uso <span className='uppercase text-sky-500 font-semibold'>Tailwind</span>.
@@ -170,7 +168,7 @@ function Home() {
               <span>Giphy API</span>
             </div>
           </article>
-          <article className='shadow-lg p-3 text-gray-500 rounded-md border-2 border-gray-200 m-3 sm:mx-20 md:mx-20 lg:mx-24 xl:mx-48'>
+          <article className='shadow-lg p-3 text-gray-500 rounded-md border-2 border-gray-200 m-3 sm:mx-20 md:mx-20 lg:mx-24 xl:mx-80'>
             <div className='flex flex-row justify-between'>
               <span className='uppercase text-left pl-3 text-gray-500 font-semibold'>Proet cursos</span>
               <div className='flex flex-row gap-2 text-xl cursor-pointer'>
@@ -183,9 +181,9 @@ function Home() {
               </div>
             </div>
             <div>
-              <figure className='flex flex-col justify-center items-center py-2'>
-                <img src={proet} className="image-project" alt="rick and morty project" />
-              </figure>
+              <a href="https://proet-e6dfc9.netlify.app/" target={"_blank"} className='flex flex-col justify-center items-center py-2 overflow-hidden'>
+                <img src={proet} className="image-project hover:scale-75" alt="rick and morty project" />
+              </a>
             </div>
             <p className='m-3'><span className='text-gray-500 uppercase font-semibold'>Proet</span> aplicacion de cursos para suscribirte a los distintos cursos.
               Sección de compras con tarjeta de credito validaciones de email y envios del mismo al finalizar la compra.
@@ -196,7 +194,7 @@ function Home() {
               <span>Abstract API</span>
             </div>
           </article>
-          <article className='shadow-lg p-3 text-gray-500 rounded-md border-2 border-gray-200 m-3 md:mx-20 sm:mx-20 lg:mx-24 xl:mx-48'>
+          <article className='shadow-lg p-3 text-gray-500 rounded-md border-2 border-gray-200 m-3 md:mx-20 sm:mx-20 lg:mx-24 xl:mx-80'>
             <div className='flex flex-row justify-between'>
               <span className='uppercase text-left pl-3 text-gray-500 font-semibold'>Kanban-app</span>
               <div className='flex flex-row gap-2 text-xl cursor-pointer'>
@@ -209,9 +207,9 @@ function Home() {
               </div>
             </div>
             <div>
-              <figure className='flex flex-col justify-center items-center py-2'>
-                <img src={kanban} className="image-project" alt="kanban project" />
-              </figure>
+              <a href="https://app.netlify.com/sites/kanban-645/overview" target={"_blank"} className='flex flex-col justify-center items-center py-2 overflow-hidden'>
+                <img src={kanban} className="image-project hover:scale-75" alt="kanban project" />
+              </a>
             </div>
             <p className='m-3'>Web app de tareas, el proyecto surgio como desafio de <span className='text-gray-500 font-semibold uppercase'>front-end mentor</span>.</p>
             <p className='pl-3'>En lo que es el desarrollo para el manejo de estilos se uso <span className='uppercase text-sky-500 font-semibold'>MUI</span>.
@@ -221,7 +219,7 @@ function Home() {
               <span>MUI</span>
             </div>
           </article>
-          <article className='shadow-lg p-3 text-gray-500 rounded-md border-2 border-gray-200 m-3 sm:mx-20 md:mx-20 lg:mx-24 xl:mx-48'>
+          <article className='shadow-lg p-3 text-gray-500 rounded-md border-2 border-gray-200 m-3 sm:mx-20 md:mx-20 lg:mx-24 xl:mx-80'>
             <div className='flex flex-row justify-between'>
               <span className='uppercase text-left pl-3 text-gray-500 font-semibold'>Rick and Morty API</span>
               <div className='flex flex-row gap-2 text-xl cursor-pointer'>
@@ -233,10 +231,10 @@ function Home() {
                 </a>
               </div>
             </div>
-            <div>
-              <figure className='flex flex-col justify-center items-center py-2'>
-                <img src={rick} className="image-project" alt="rick and morty project" />
-              </figure>
+            <div>             
+              <a href="https://magenta-hummingbird-5754c9.netlify.app/" target={"_blank"} className='flex flex-col justify-center items-center py-2 overflow-hidden'>
+                <img src={rick} className="image-project hover:scale-75" alt="rick and morty project"/>
+              </a>
             </div>
             <p className='m-3'>Web app para ver los personajes de <span className='text-gray-500 uppercase font-semibold'>Rick and morty</span> y sus episodios y fechas de  estreno, para las llamadas a la API se hizo uso de <span className='text-gray-500 font-semibold uppercase'>AXIOS</span>.</p>
             <p className='pl-3'>para el manejo de estilos se uso <span className='uppercase text-sky-500 font-semibold'>Tailwind</span>.
@@ -288,24 +286,24 @@ function Home() {
             }) => (
               <form className="space-y-8 mb-7 mx-1" onSubmit={handleSubmit}>
                 <div>
-                  <label htmlFor="name" className="block mb-2 text-sm dark:text-gray-400" >Nombre</label>
+                  <label htmlFor="name" className="block mb-2 text-sm  text-gray-400" >Nombre</label>
                   <input type="text" id="name" onBlur={handleBlur} value={values.name} onChange={handleChange} className={`block p-3 w-full text-sm  bg-gray-50 rounded-lg shadow-sm outline-none border`} placeholder="Ingrese su nombre" name="name" required />
-                  <label className={`${touched.name && errors.name ? 'error-text  text-sm font-semibold text-red-500' : 'hidden'}`}>{touched.name && errors.name}</label>
+                  <label className={`${touched.name && errors.name ? 'error-text  text-sm font-semibold text-red-400' : 'hidden'}`}>{touched.name && errors.name}</label>
                 </div>
                 <div>
-                  <label htmlFor="email" className={`block mb-2 text-sm  text-gray-900 dark:text-gray-300`}>Email</label>
+                  <label htmlFor="email" className={`block mb-2 text-sm  text-gray-400`}>Email</label>
                   <input type="email" id="email" onChange={handleChange} onBlur={handleBlur} value={values.email} className={`shadow-sm bg-gray-50  text-gray-900 text-sm rounded-lg outline-none block w-full p-2.5 border border-gray-300`} placeholder="nombre@gmail.com" required name="email" />
-                  <label className={`${touched.email && errors.email ? 'error-text  text-sm font-semibold text-red-500' : 'hidden'}`}>{touched.email && errors.email}</label>
+                  <label className={`${touched.email && errors.email ? 'error-text  text-sm font-semibold text-red-400' : 'hidden'}`}>{touched.email && errors.email}</label>
                 </div>
                 <div className="sm:col-span-2">
-                  <label htmlFor="message" className="block mb-2 text-sm  text-gray-900 dark:text-gray-400" >Mensage</label>
+                  <label htmlFor="message" className="block mb-2 text-sm  text-gray-400" >Mensage</label>
                   <textarea id="message" rows="6" onChange={handleChange} onBlur={handleBlur} value={values.message} className={`block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm outline-none border border-gray-300 `} placeholder="Tu mensaje" required name="message"></textarea>
-                  <label className={`${touched.message && errors.message ? 'error-text  text-sm font-semibold text-red-500' : 'hidden'}`}>{touched.message && errors.message}</label>
+                  <label className={`${touched.message && errors.message ? 'error-text  text-sm font-semibold text-red-400' : 'hidden'}`}>{touched.message && errors.message}</label>
                 </div>
                 {
                   !errors.name && !errors.email && !errors.message &&
                   <div className='flex justify-center'>
-                    <button type="submit" disabled={state.submitting} className={`${!errors.name && !errors.email && !errors.message ? 'submit' : 'hidden'} py-1 px-24 text-lg font-medium text-center  border-2 border-slate-400 hover:bg-slate-200 hover:transition-transform transition-all text-zinc-400 rounded-md bg-primary-70 outline-none`}>Enviar</button>
+                    <button type="submit" disabled={state.submitting} className={`${!errors.name && !errors.email && !errors.message ? 'submit' : 'hidden'} py-1 px-24 text-lg font-medium text-center rounded-md outline-none form__submit`}>Enviar</button>
                   </div>
                 }
               </form>
